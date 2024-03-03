aecb750c382c16a431b6636da0bf076c45a1b915
Vincent Rodriguez <vdsrodriguez@gmail.com>
2024-03-03 18:38:02 -0500

    first touch with UI

--- app/src/main/java/com/santiagorodriguez/wise/MainActivity.kt
+++ app/src/main/java/com/santiagorodriguez/wise/MainActivity.kt
@@ -2,10 +2,23 @@ package com.santiagorodriguez.wise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.santiagorodriguez.wise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var card = binding.TestCard
+
+        card.setOnLongClickListener {
+            card.setChecked(!card.isChecked)
+            true
+        }
+
+
     }
 }
